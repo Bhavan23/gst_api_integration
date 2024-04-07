342227d6bfd39768d18ca1d4ac032a4dcfdaa708
Bhavan23 <bhavansathru.it@gmail.com>
2024-04-07 19:51:10 +0530

    fix: gstr2 report added with filter

--- gst_api_integration/gst_api_integration/report/gstr_2_custom/gstr_2_custom.js
+++ gst_api_integration/gst_api_integration/report/gstr_2_custom/gstr_2_custom.js
@@ -17,15 +17,24 @@ frappe.query_reports["GSTR-2-Custom"] = {
 			"label": __("From Date"),
 			"fieldtype": "Date",
 			"reqd": 1,
-			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -3),
-			"width": "80"
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"width": "80",
+			change: function(){
+				var from_date = new Date(frappe.query_report.get_filter_value('from_date')); 
+
+				var year = from_date.getFullYear()
+				var month = from_date.getMonth(); 
+
+				var to_date = new Date(year, month + 1, 0);
+
+				frappe.query_report.set_filter_value('to_date', to_date)
+			}
 		},
 		{
 			"fieldname":"to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
-			"reqd": 1,
-			"default": frappe.datetime.get_today()
+			"read_only": 1
 		},
 		{
 			"fieldname":"type_of_business",
@@ -36,14 +45,19 @@ frappe.query_reports["GSTR-2-Custom"] = {
 			"default": "B2B"
 		},
 		{
-			"fieldname":"filed",
-			"label": __("Filed only"),
-			"fieldtype": "Check"
+			"fieldname":"record_from",
+			"label": __("Show"),
+			"fieldtype": "Select",
+			"default": "System Data",
+			"options":['', 'System Data', 'Portal Data']
 		},
 		{
-			"fieldname":"not_filed",
-			"label": __("Not Filed Only"),
-			"fieldtype": "Check"
+			"fieldname":"filed",
+			"label": __("Filed"),
+			"fieldtype": "Check",
+			change: function(){
+				frappe.query_report.set_filter_value('record_from', '')
+			},
 		}
 	]
 };
--- gst_api_integration/gst_api_integration/report/gstr_2_custom/gstr_2_custom.py
+++ gst_api_integration/gst_api_integration/report/gstr_2_custom/gstr_2_custom.py
@@ -7,7 +7,7 @@ from erpnext.regional.report.gstr_2.gstr_2 import Gstr2Report
 from gst_api_integration.gst_api_integration.doctype.gstr_1_report.gstr_1_report import get_access_token
 from gst_api_integration.gst_api_integration.doctype.api_log.api_log import create_api_log
 from random import randint
-from frappe.utils import getdate, random_string, cstr, flt
+from frappe.utils import getdate, random_string, cstr, flt, cint
 from frappe import _
 import json
 import requests
@@ -18,53 +18,84 @@ ACTIONS = {
 	}
 
 def execute(filters=None):
-	global datas
-	columns, datas = Gstr2Report(filters).run()
+	try:
+		columns, datas = Gstr2Report(filters).run()
+	except Exception as e:
+		if "no attribute 'tax_details'" in cstr(e):
+			frappe.throw("Selct Different Date", title= "Invalid Date")
+
 	action = ACTIONS.get(filters.get('type_of_business'))
 	
-	if filters.get('filed'):
-		columns = get_gst_columns()
-		datas = []
-  
-		for data in  get_gstr2_data(filters, action=ACTIONS.get(action)) or []:
-			gstin = [data.get('ctin'),]
-			for inv in data.get('inv'):
-				idt = str(frappe.utils.getdate(inv.get('idt')))
-				
-				main_value = [
-					inv.get('inum'), idt, flt(inv.get('val'), 2)
-				]
-				datas.append(gstin + main_value)
+	portal_data = []
+	if filters.get('record_from') != 'System Data':
+		portal_data = get_portal_data(filters, action=ACTIONS.get(action))
 
-	return columns, datas
+	gstins = [data[0] for data in get_gst_formated_data(portal_data)]
+	invoices = [data[1] for data in get_gst_formated_data(portal_data)]
 
-def filter_data(mapped_data, gst_data, filed):
-	
-	row_map = {row: data for row, data in enumerate(datas)}
- 
-	def get_value(data):
-		data[1:2] = [None]
-		return data
-
-	with_out_invoice = [get_value(list(data)) for data in gst_data]
-	
-	for key in mapped_data:
-		
-		if filed:
-			print(mapped_data[key] ,  gst_data)
-			if mapped_data[key] not in gst_data:
-				print(mapped_data[key],"\n",)
-				if mapped_data[key] not in with_out_invoice:
-					row_map.pop(key)
+	if not filters.get('record_from'):
+		if filters.get('filed'):
+			datas = filter_filed_data(gstins, invoices, datas)
 		else:
-			if mapped_data[key] in gst_data:
-				if mapped_data[key] in with_out_invoice:
-					row_map.pop(key)
-     
-	return list(row_map.values())
+			filed_data = filter_filed_data(gstins, invoices, datas)
+			for data in filed_data:
+				datas.remove(data)
+
+		return columns, datas
+	else:
+		if filters.get('record_from') == 'Portal Data':
+			columns = get_gst_columns()
+			datas = []
+			for data in  get_portal_data(filters, action=ACTIONS.get(action)) or []:
+				gstin = [data.get('ctin'),]
+				for inv in data.get('inv'):
+					idt = str(frappe.utils.getdate(inv.get('idt')))
+
+					main_value = [
+						inv.get('inum'), idt, flt(inv.get('val'), 2)
+					]
+					datas.append(gstin + main_value)
+
+		return columns, datas
+
+def filter_filed_data(gstins, invoices, system_data):
+	filed = False
+	filed_data = []
+	for data in system_data:
+		if data[0] in gstins:
+			filed = True
+		if filed:
+			if bill_no:=check_exists(data[1]):
+				if bill_no in invoices:
+					filed = True
+				else:
+					for invoice in invoices:
+						if  invoice in bill_no:
+							filed = True
+							break
+					else:
+						filed = False
+			else:
+				filed = False
+		if filed:
+			filed_data.append(data)
+
+	return filed_data
+def get_gst_formated_data(datas):
+	new_datas = []
+	for data in  datas:
+			gstin = data.get('ctin')
+			for inv in data.get('inv'):
+				new_datas.append([gstin, inv.get('inum')])
+    
+	return new_datas
 
 
-def get_gstr2_data(filters, action):
+def check_exists(invoice_no):
+	if frappe.db.get_value("Purchase Invoice", invoice_no, 'bill_no'):
+		return frappe.db.get_value("Purchase Invoice", invoice_no , 'bill_no')
+
+def get_portal_data(filters, action):
 	gst_integration_settings = frappe.get_single('GST Integration Settings')
 
 	# GST integration Details
@@ -102,13 +133,13 @@ def get_gstr2_data(filters, action):
 
 	headers = {
 			'username':  user_name,
-			  'state-cd': state_code,
-			  'otp': otp,
-			  'Content-Type': content_type,
-			  'requestid': requestid,
-			  'gstin': gstin,
-			  'ret_period': ret_period,
-			  'Authorization': "Bearer " + access_token
+			'state-cd': state_code,
+			'otp': otp,
+			'Content-Type': content_type,
+			'requestid': requestid,
+			'gstin': gstin,
+			'ret_period': ret_period,
+			'Authorization': "Bearer " + access_token
 		}
 
 	response = requests.request(
@@ -118,14 +149,12 @@ def get_gstr2_data(filters, action):
 
 	if response.ok:
 		res = response.json()
-		if res.get('status') == 200:
+		if cint(res.get('status')) == 200:
 			return res.get(action.lower())
 		else:
-			frappe.throw(res.get('message'), title="BAD Response")
+			frappe.msgprint(res.get('message'), title="BAD Response")
 	else:
-		frappe.throw("{}".format(response), title= "Api Error")
-
-
+		frappe.throw("{}".format(cstr(response.json().get('error_description'))), title= "Api Error")
 
 def get_gst_columns():
 	return [
@@ -141,15 +170,17 @@ def get_gst_columns():
 			"fieldtype": "Data" 
 		},
 		{
-			"fieldname":"Date",
+			"fieldname":"date",
 			"label": _("Date"),
 			"fieldtype": "Date" 
 		},
 		{
 			"fieldname":"amount",
 			"label": _("Amount"),
-			"fieldtype": "float" ,
-			"width": "100px"
+			"fieldtype": "Float" ,
+			"precision": 2,
+			"width": "100px",
+			"width": 200
 		},
-	]        
+	]
 	
